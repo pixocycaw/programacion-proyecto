8f100a2347fd4a9d4b1fbc4a6a7424a86f7a5c72
unknown <smu0005@alu.medac.es>
2025-04-04 12:04:48 +0200

    Añadir tabla Detalles_Pedido

--- base-de-datos-y-tablas.sql
+++ base-de-datos-y-tablas.sql
@@ -1,4 +1,4 @@
--- Crear y usar una base de datos.
+-- Crear y usar la base de datos
 CREATE DATABASE tienda;
 USE tienda;
 
@@ -8,10 +8,10 @@ USE tienda;
 
 -- Clientes
 CREATE TABLE clientes (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
-	email VARCHAR(255) NOT NULL UNIQUE, 
+    email VARCHAR(255) NOT NULL UNIQUE, 
     telefono VARCHAR(15) UNIQUE,
     dni VARCHAR(9) NOT NULL UNIQUE,
     direccion VARCHAR(255) NOT NULL
@@ -19,7 +19,7 @@ CREATE TABLE clientes (
 
 -- Productos
 CREATE TABLE productos (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10, 2) NOT NULL,
@@ -28,13 +28,21 @@ CREATE TABLE productos (
 
 -- Pedidos
 CREATE TABLE pedidos (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     estado ENUM('pendiente', 'procesado', 'enviado', 'cancelado') DEFAULT 'pendiente',
     fecha DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    cantidad INT NOT NULL,
     precio_total DECIMAL(10, 2) NOT NULL,
     id_cliente INT NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id) ON DELETE CASCADE
+);
+
+-- Detalles_Pedido
+CREATE TABLE detalles_pedido (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_pedido INT NOT NULL,
     id_producto INT NOT NULL,
-	FOREIGN KEY (id_cliente) REFERENCES Clientes(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_producto) REFERENCES Productos(id) ON DELETE CASCADE
+    cantidad INT NOT NULL,
+    precio_subtotal DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (id_pedido) REFERENCES pedidos(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_producto) REFERENCES productos(id) ON DELETE CASCADE
 );
