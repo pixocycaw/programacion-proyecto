3aa16bb18f6ceac7e2efcb3b92e9f1009558c050
unknown <smu0005@alu.medac.es>
2025-04-07 12:53:33 +0200

    Añadir consulta 20

---
+++ consultas/4. Consultas con Fechas y Cálculos/consulta_20.sql
@@ -0,0 +1,14 @@
+-- Crear la vista
+CREATE VIEW productos_mas_de_20_unidades
+AS
+SELECT
+	* 
+FROM
+	productos
+WHERE
+	stock > 20
+ORDER BY
+	stock DESC;
+     
+-- Abrir la vista
+SELECT * FROM productos_mas_de_20_unidades;
