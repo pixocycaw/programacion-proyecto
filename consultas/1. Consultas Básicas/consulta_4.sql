db35938a66ec6d64e2135a045ca4b06927d04b7b
unknown <smu0005@alu.medac.es>
2025-04-07 10:03:56 +0200

    Cambiar consulta 4

--- consultas/1. Consultas Básicas/consulta_4.sql
+++ consultas/1. Consultas Básicas/consulta_4.sql
@@ -1,1 +1,16 @@
-SELECT * FROM detalles_pedido;
+SELECT 
+    dp.id_pedido,
+    c.nombre AS cliente,
+    p.nombre AS nombre_producto,
+    dp.cantidad,
+    dp.precio_subtotal
+FROM 
+    detalles_pedido dp
+JOIN 
+    productos p ON dp.id_producto = p.id
+JOIN 
+    pedidos ped ON dp.id_pedido = ped.id
+JOIN
+	clientes c ON ped.id_cliente = c.id
+ORDER BY 
+    dp.precio_subtotal DESC;
