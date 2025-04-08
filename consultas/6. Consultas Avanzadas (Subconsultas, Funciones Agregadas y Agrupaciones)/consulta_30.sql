6e592447e9857b933fd3fd92aa23cf244193c56c
unknown <smu0005@alu.medac.es>
2025-04-08 12:25:45 +0200

    Cambiar consulta 30

--- consultas/6. Consultas Avanzadas (Subconsultas, Funciones Agregadas y Agrupaciones)/consulta_30.sql
+++ consultas/6. Consultas Avanzadas (Subconsultas, Funciones Agregadas y Agrupaciones)/consulta_30.sql
@@ -2,28 +2,25 @@
 CREATE VIEW pedidos_con_productos_y_clientes
 AS
 SELECT 
-    ped.id AS id_pedido,
-    ped.fecha AS fecha_pedido,
-    ped.estado AS estado_pedido,
-    ped.precio_total AS precio_total_pedido,
-    c.id AS id_cliente,
-    c.nombre AS nombre_cliente,
-    c.apellidos AS apellidos_cliente,
-    c.direccion AS direccion_cliente,
-    dp.id_producto AS id_producto,
-    p.nombre AS nombre_producto,
-    dp.cantidad AS cantidad_producto,
-    dp.precio_subtotal AS precio_subtotal_producto
-FROM 
-    pedidos ped
-JOIN 
-    clientes c ON ped.id_cliente = c.id
-JOIN 
-    detalles_pedido dp ON ped.id = dp.id_pedido
-JOIN 
-    productos p ON dp.id_producto = p.id
-ORDER BY 
-    ped.id, dp.id;
+	p.id AS id_pedido,
+	(SELECT c.nombre
+	 FROM clientes c 
+	 WHERE c.id = p.id_cliente) AS cliente,
+	p.fecha,
+    p.estado,
+	p.precio_total,
+	(SELECT GROUP_CONCAT(
+		 pr.nombre
+		 SEPARATOR ', '
+     )
+	 FROM
+		 detalles_pedido dp
+	 JOIN
+		 productos pr ON dp.id_producto = pr.id
+	 WHERE
+		 dp.id_pedido = p.id
+    ) AS productos_comprados
+FROM pedidos p;
      
 -- Abrir la vista
 SELECT * FROM pedidos_con_productos_y_clientes;
